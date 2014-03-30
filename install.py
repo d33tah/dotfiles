718b873de93ecd15d3db1232425e929b408a73d2
Jacek Wielemborek <d33tah@gmail.com>
2014-03-30 19:16:15 +0200

    Make the installer work with dead symlinks and .dotfile_ already existing.

--- install.py
+++ install.py
@@ -6,12 +6,28 @@ import shutil
 
 argv0 = os.path.abspath(sys.argv[0])
 
+def file_exists(path):
+    try:
+        os.stat(path)
+        return True
+    except OSError:
+        # we got a "file does not exist". perhaps it's a broken link?
+        try:
+            os.readlink(path)
+            return True
+        except OSError:
+            pass
+    return False
+
 for filename in os.listdir('.'):
     if os.path.abspath(filename) == argv0 or filename.startswith('.'):
         continue
     target = os.environ['HOME'] + os.sep + '.' + filename
     print(target)
-    if os.path.exists(target):
-        shutil.move(target, target + "_")
+    if file_exists(target):
+        new_target = target + "_"
+        while file_exists(new_target):
+            new_target = new_target + "_"
+        shutil.move(target, new_target)
     source = os.path.abspath(os.path.curdir) + os.sep + filename
     os.symlink(source, target)
